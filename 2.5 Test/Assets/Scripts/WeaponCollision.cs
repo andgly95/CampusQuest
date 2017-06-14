9c994f1ab5723dddb54b8c9d0fff55493f75fdc8
Andrew Glyadchenko <andrewgly95@gmail.com>
2017-06-14 19:30:00 -0400

    Updated enemy controller

---
+++ 2.5 Test/Assets/Scripts/WeaponCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IsoTools.Physics;
+
+public class WeaponCollision : MonoBehaviour {
+	
+	void Start() {
+		Debug.Log("Start");
+		//GroundCheckPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.2f);
+	}
+
+	void OnIsoCollisionEnter(IsoCollision iso_collision){
+
+		if(iso_collision.gameObject.tag == "Enemy"){ 
+			Debug.Log ("Hit");
+		}
+	}
+}
